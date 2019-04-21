103f12016fc4f2b69a1c026e78553444d317b946
Eliot Howes <ejhowes@gmail.com>
2019-04-21 08:50:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  else ("I love you, Grandma" === string) {
+  else {
     return "I love you, too."
   };
 };
