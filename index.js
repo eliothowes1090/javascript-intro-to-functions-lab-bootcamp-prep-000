da3f9117011c8a17944cff835891dfcae6875da5
Eliot Howes <ejhowes@gmail.com>
2019-04-21 08:32:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-  return "I can't hear you!"
+    return "I can't hear you!"
+  }
+  elseif (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else (string === "I love you, Grandma") {
+    return "I love you, too."
   }
 };
+
+
+
